b1e8d2fdae5af0baa245993aef592b95477224bf
Alexander Pajer <alexander.pajer@integrations.at>
2021-02-27 21:06:04 +0100

    setup-delete

--- 00-Setup/WSL/enable-wsl.ps1
+++
@@ -1,7 +0,0 @@
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
-
-wsl --set-default-version 2
-
-
-choco install wsl-ubuntu-1804
