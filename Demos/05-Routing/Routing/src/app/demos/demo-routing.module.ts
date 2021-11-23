05476ccfe0c9d857d71d8d65b4a28e6b9943a13e
Alexander Pajer <alexander.pajer@integrations.at>
2021-11-23 15:38:45 +0100

    routes in demo module to external file

--- Demos/05-Routing/Routing/src/app/demos/demo-routing.module.ts
+++ Demos/05-Routing/Routing/src/app/demos/demo-routing.module.ts
@@ -1,13 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { MaterialModule } from '../material.module';
-import { SharedModule } from '../shared/shared.module';
+import { Routes } from '@angular/router';
 import { DemoContainerComponent } from './demo-container/demo-container.component';
-import { demoRoutes } from './demo-routing.module';
-import { DemoService } from './demo.service';
 import { ChildRoutesComponent } from './samples/child-routes/child-routes.component';
 import { ComponentLessComponent } from './samples/component-less/component-less.component';
 import { LazyLoadingComponent } from './samples/lazy-loading/lazy-loading.component';
@@ -21,30 +13,29 @@ import { RouterStateComponent } from './samples/router-state/router-state.compon
 import { RoutingBasicsComponent } from './samples/routing-basics/routing-basics.component';
 import { SecondaryRoutesComponent } from './samples/secondary-routes/secondary-routes.component';
 
-@NgModule({
-  declarations: [
-    DemoContainerComponent,
-    RoutingBasicsComponent,
-    ChildRoutesComponent,
-    SecondaryRoutesComponent,
-    RouteGuardsComponent,
-    PreloadComponent,
-    ParamMapComponent,
-    PmChildComponent,
-    ComponentLessComponent,
-    LazyLoadingComponent,
-    RouterStateComponent,
-    RouterAnimationsComponent,
-    RouterEventsComponent,
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild(demoRoutes),
-    MaterialModule,
-    SharedModule,
-    HttpClientModule,
-  ],
-  providers: [DemoService],
-})
-export class DemosModule {}
+export const demoRoutes: Routes = [
+  {
+    path: '',
+    component: DemoContainerComponent,
+    children: [
+      { path: 'routingbasics', component: RoutingBasicsComponent },
+      {
+        path: 'parammap',
+        component: ParamMapComponent,
+        children: [{ path: ':id', component: PmChildComponent }],
+      },
+      { path: 'router-state', component: RouterStateComponent },
+      { path: 'childroutes', component: ChildRoutesComponent },
+      { path: 'secondary', component: SecondaryRoutesComponent },
+      { path: 'routeguards', component: RouteGuardsComponent },
+      { path: 'preload', component: PreloadComponent },
+      { path: 'lazy-loading', component: LazyLoadingComponent },
+      { path: 'events', component: RouterEventsComponent },
+      { path: 'router-animation', component: RouterAnimationsComponent },
+      {
+        path: 'component-less',
+        component: ComponentLessComponent,
+      },
+    ],
+  },
+];
