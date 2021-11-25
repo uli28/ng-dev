537630ad967f6da8739269d6775f2b2cbfac1794
Alexander Pajer <alexander.pajer@integrations.at>
2021-11-25 13:02:37 +0100

    reactive bug fix

--- Demos/08-Reactive-Programming-and-State/Reactive/src/environments/environment.prod.ts
+++ Demos/08-Reactive-Programming-and-State/Reactive/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  authEnabled: false,
+  title: 'ngReactive',
+  markdownPath: '/assets/markdown/',
+  apiUrl: 'http://localhost:3000/vouchers',
+  api: 'http://localhost:3000/',
 };
