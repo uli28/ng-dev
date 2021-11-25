6ee1d0036fc4fb2c03aa877d5f0904bb541df735
Alexander Pajer <alexander.pajer@integrations.at>
2021-11-25 08:34:01 +0100

    tests

--- Labs/L7-Testing/food-app/src/app/food/food-container/food-container.component.spec.ts
+++ Labs/L7-Testing/food-app/src/app/food/food-container/food-container.component.spec.ts
@@ -1,11 +1,15 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
+import { MaterialModule } from '../../material.module';
+import { FoodEditComponent } from '../food-edit/food-edit.component';
 import { FoodListComponent } from '../food-list/food-list.component';
 import { FoodService } from '../food.service';
+import { FoodContainerComponent } from './food-container.component';
 
-describe('Integration Test:', () => {
+describe('food-list-container', () => {
   let mockFS;
   const foodData = [
     { id: 1, name: 'Butter Chicken', price: 9, calories: 1200 },
@@ -16,28 +20,41 @@ describe('Integration Test:', () => {
     { name: 'Pad Thai', rating: 5 },
     { name: 'Butter Chicken', rating: 5 },
   ];
-  let fixture: ComponentFixture<FoodListComponent>;
+  let component: FoodContainerComponent;
+  let fixture: ComponentFixture<FoodContainerComponent>;
 
   beforeEach(() => {
-    mockFS = jasmine.createSpyObj(['getFood', 'deleteFood']);
+    mockFS = jasmine.createSpyObj([
+      'getFood',
+      'deleteFood',
+      'addFood',
+      'updateFood',
+    ]);
 
     const module = {
-      declarations: [FoodListComponent],
+      declarations: [FoodListComponent, FoodEditComponent],
+      import: [MaterialModule, FormsModule, ReactiveFormsModule, CommonModule],
       providers: [{ provide: FoodService, useValue: mockFS }],
-      schemas: [NO_ERRORS_SCHEMA],
     };
 
     TestBed.configureTestingModule(module);
-    fixture = TestBed.createComponent(FoodListComponent);
+    fixture = TestBed.createComponent(FoodContainerComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should render each FoodItem as FoodItemRow', () => {
-    mockFS.getItems.and.returnValue(of(foodData));
-    mockFS.deleteItem.and.returnValue(of(serviceResult));
+  it('should render the food-list.component', () => {
+    mockFS.getFood.and.returnValue(of(foodData));
     fixture.detectChanges();
 
-    const rows = fixture.debugElement.queryAll(By.directive(FoodListComponent));
-    expect(rows.length).toEqual(1);
-    // expect(rows[0].componentInstance.food.name).toEqual('Pad Thai');
+    const fl = fixture.debugElement.queryAll(By.directive(FoodListComponent));
+    expect(fl).toBeDefined();
+  });
+
+  it('should render the food-edit.component', () => {
+    mockFS.getFood.and.returnValue(of(foodData));
+    fixture.detectChanges();
+
+    const fl = fixture.debugElement.queryAll(By.directive(FoodEditComponent));
+    expect(fl).toBeDefined();
   });
 });
