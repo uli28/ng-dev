60eb9b2abd1e2c43902e43784ca537c030921393
Alexander Pajer <alexander.pajer@integrations.at>
2022-04-25 11:41:08 +0200

    ng13 update

--- Demos/01-Intro/first-ng13/src/app/app.module.ts
+++ Demos/01-Intro/first-ng13/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
