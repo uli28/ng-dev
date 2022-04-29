08b320a2981d25298e5bab7c8e4f17d4d02e4089
Alexander Pajer <alexander.pajer@integrations.at>
2022-04-29 07:47:55 +0200

    setup

--- Setup/setup-angular-dev.ps1
+++ Setup/setup-angular-dev.ps1
@@ -5,16 +5,17 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install chrome-remote-desktop-chrome -y
+choco install googlechrome -y
 choco install microsoft-edge -y
 choco install vscode -y
-choco install dotnetcore-sdk -y
+choco install dotnet-6.0-sdk -y
+choco install nodejs-lts --version=14.18.0 -y
 choco install azure-cli -y
 choco install git -y
+choco install gh -y
 choco install gitextensions -y
 choco install curl -y
 choco install 7zip -y
-choco install nvm -y
 
 # Intall VS Code Extensions
 Write-Host "VS Code Extensions - 5/6" -ForegroundColor yellow
@@ -25,13 +26,13 @@ code --install-extension ms-vscode.azure-account
 code --install-extension ms-azuretools.vscode-azureappservice
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension GitHub.vscode-pull-request-github
+code --install-extension mhutchie.git-graph 
+code --install-extension mdickin.markdown-shortcuts
 code --install-extension redhat.vscode-yaml
 code --install-extension angular.ng-template
 code --install-extension 1tontech.angular-material
 code --install-extension mikael.angular-beastcode
-code --install-extension CoenraadS.bracket-pair-colorizer-2
-code --install-extension mdickin.markdown-shortcuts
-code --install-extension mhutchie.git-graph 
+code --install-extension xabikos.javascript
 code --install-extension hbenl.vscode-test-explorer
 code --install-extension raagh.angular-karma-test-explorer
 code --install-extension connorshea/vscode-test-explorer-status-bar
@@ -41,10 +42,6 @@ code --install-extension wallabyjs.wallaby-vscode
 Write-Host "Refresh Path Env - 4/6" -ForegroundColor yellow
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-
-nvm install 12.18.1
-nvm install 14.17.5
-nvm use 14.17.5
 
 # Install Angular
 Write-Host "Installing Angular - 6/6" -ForegroundColor yellow
