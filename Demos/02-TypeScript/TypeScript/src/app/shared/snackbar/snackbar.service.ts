f306e73106e85a0c27369cfa4c42044d3421f670
Alexander Pajer <alexander.pajer@integrations.at>
2022-04-30 09:53:30 +0200

    snackbar

--- Demos/02-TypeScript/TypeScript/src/app/shared/snackbar/snackbar.service.ts
+++ Demos/02-TypeScript/TypeScript/src/app/shared/snackbar/snackbar.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SnackbarService {
-  constructor(private snackBar: MatSnackBar) {}
+  config: MatSnackBarConfig;
+
+  constructor(private snackBar: MatSnackBar) {
+    this.config = new MatSnackBarConfig();
+    this.config.duration = 1000;
+    this.config.panelClass = ['app-snackbar'];
+  }
 
   displayAlert(title: string, msg: string) {
-    this.snackBar.open(title, msg, {
-      duration: 1000,
-    });
+    this.snackBar.open(title, msg, this.config);
   }
 }
