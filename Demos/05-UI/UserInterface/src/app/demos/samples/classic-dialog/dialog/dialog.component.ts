10194188d859e5512684f10e012a4016138f44a3
Alexander Pajer <alexander.pajer@integrations.at>
2022-05-01 10:07:53 +0200

    isses create for mod 5

--- Demos/05-UI/UserInterface/src/app/demos/samples/classic-dialog/dialog/dialog.component.ts
+++ Demos/05-UI/UserInterface/src/app/demos/samples/classic-dialog/dialog/dialog.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { ClassicDialogComponent } from '../classic-dialog.component';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
 })
-export class DialogComponent implements OnInit {
+export class DialogComponent {
   constructor(
     public dialogRef: MatDialogRef<ClassicDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { name: string }
   ) {}
 
-  ngOnInit() {}
+  fcName = new FormControl(this.data.name, [Validators.required]);
 }
