8f2b82c2a932e703cada11af18138d88b7e46e6d
Alexander Pajer <alexander.pajer@integrations.at>
2022-05-04 20:12:20 +0200

    lab-8

---
+++ Labs/L8-Security/food-app-l8-starter/src/app/food/food-container/food-container.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodItem } from '../food.model';
+import { FoodService } from '../food.service';
+
+@Component({
+  selector: 'app-food-container',
+  templateUrl: './food-container.component.html',
+  styleUrls: ['./food-container.component.scss'],
+})
+export class FoodContainerComponent implements OnInit {
+  food: FoodItem[];
+  selected: FoodItem | null;
+
+  constructor(private fs: FoodService) {}
+
+  ngOnInit() {
+    this.fs.getFood().subscribe((data) => (this.food = data));
+  }
+
+  addFood() {
+    this.selected = { id: 0, name: '', price: 0, calories: 0 } as FoodItem;
+  }
+
+  selectFood(f: FoodItem) {
+    this.selected = { ...f };
+  }
+
+  deleteFood(f: FoodItem) {
+    this.fs.deleteFood(f.id).subscribe(() => {
+      let deleted = this.food.filter((item) => item.id != f.id);
+      this.food = [...deleted];
+      this.selected = null;
+    });
+  }
+
+  saveFood(f: FoodItem) {
+    if (f.id) {
+      this.fs.updateFood(f).subscribe((result) => {
+        let existing = this.food.find((f) => f.id == result.id);
+        Object.assign(existing, result);
+        this.food = [...this.food];
+      });
+    } else {
+      this.fs.addFood(f).subscribe((result) => {
+        this.food.push(result);
+        this.food = [...this.food];
+      });
+    }
+  }
+}
--- Labs/L8-Security/readme.md
+++ Labs/L8-Security/readme.md
@@ -1,1 +1,17 @@
-# Food App - Security
+# Food App - Security & Publishing
+
+## Security
+
+- Create a new project in the [Firebase Console](https://console.firebase.google.com/)
+
+- Add a new Web App
+
+    ![add-app](_images/add-app.jpg)
+
+    ![web-app](_images/web-app.jpg)
+
+- Copy the configuration to `environment.ts`    
+
+    ![web-app](_images/config.jpg)
+
+- Implement Authentication. Use this [reference](/Demos/10-Securing-Publishing/demo-ui)
