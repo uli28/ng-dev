16bac5c4b857d6289d12f48e13492f01213b65fd
Alexander Pajer <alexander.pajer@integrations.at>
2022-05-08 10:08:08 +0200

    component class

--- Demos/09-Testing/Testing/src/app/demos/samples/component-class/component-class.component.spec.ts
+++ Demos/09-Testing/Testing/src/app/demos/samples/component-class/component-class.component.spec.ts
@@ -1,12 +1,27 @@
 import { ComponentClassComponent } from './component-class.component';
+import { foodAddItem } from '../foodService/food.mocks';
+import { FoodItem } from '../foodService/food.model';
 
 describe('ComponentClassComponent', () => {
   let component: ComponentClassComponent;
 
-  it('should create', () => {
+  beforeEach(() => {
     component = new ComponentClassComponent();
+  });
+
+  it('should create and setup correctly', () => {
     expect(component).toBeTruthy();
     component.ngOnInit();
     expect(component.title).toBe('Food App');
+    expect(component.food.length).toEqual(2);
+  });
+
+  it('should have 3 items when an item is added', () => {
+    let item: FoodItem = { id: 4, name: 'Blini with Salmon', rating: 1 };
+    component.ngOnInit();
+    expect(component.food.length).toEqual(2);
+    component.add(item);
+    expect(component.food.length).toEqual(3);
+    expect(component.food[2]).toEqual(item);
   });
 });
--- Demos/09-Testing/Testing/src/app/demos/samples/component-class/component-class.component.ts
+++ Demos/09-Testing/Testing/src/app/demos/samples/component-class/component-class.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodItem } from '../foodService/food.model';
 
 @Component({
   selector: 'app-component-class',
@@ -11,16 +12,26 @@ import { Component, OnInit } from '@angular/core';
       </mat-card-header>
       <mat-card-content>
         <h1>{{ title }}</h1>
+        <div *ngFor="let f of food">{{ f.name }}</div>
       </mat-card-content>
     </mat-card>`,
   styles: ['h1 { color: green; font-size: 2rem}'],
 })
 export class ComponentClassComponent implements OnInit {
   title = '';
+  food: FoodItem[];
 
   constructor() {}
 
   ngOnInit(): void {
     this.title = 'Food App';
+    this.food = [
+      { id: 2, name: 'Pad Thai', rating: 1 },
+      { id: 3, name: 'Butter Chicken', rating: 2 },
+    ];
+  }
+
+  add(f: FoodItem) {
+    this.food.push(f);
   }
 }
