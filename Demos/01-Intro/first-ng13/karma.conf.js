07bd6c18aba8673ae9e9083d274bc8c0f6a6ccac
Alexander Pajer <alexander.pajer@integrations.at>
2022-05-09 12:37:31 +0200

    live demo update

--- Demos/01-Intro/first-ng13/karma.conf.js
+++ Demos/01-Intro/first-ng13/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/first-ng13'),
+      dir: require('path').join(__dirname, './coverage/first-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Demos/01-Intro/first-ng13/src/app/app.module.ts
+++ Demos/01-Intro/first-ng13/src/app/app.module.ts
@@ -3,22 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    NavbarComponent,
     SidebarComponent,
-    NavbarComponent
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
