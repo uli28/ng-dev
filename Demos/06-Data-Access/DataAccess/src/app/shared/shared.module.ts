24cf2baa7e9b5adaf63e675acae30100cf2ea4f6
Alexander Pajer <alexander.pajer@integrations.at>
2022-05-11 15:10:04 +0200

    shared module fix

--- Demos/06-Data-Access/DataAccess/src/app/shared/shared.module.ts
+++ Demos/06-Data-Access/DataAccess/src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { MarkdownModule } from 'ngx-markdown';
 import { MaterialModule } from '../material.module';
 import { FooterComponent } from './footer/footer.component';
-import { IntroComponent } from './intro/intro.component';
-import { MarkdownRendererComponent } from './markdown-renderer/markdown-renderer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidePanelComponent } from './side-panel/side-panel.component';
+import { MarkdownRendererComponent } from './markdown-renderer/markdown-renderer.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { HttpClient } from '@angular/common/http';
+import { IntroComponent } from './intro/intro.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 const mods = [
@@ -18,6 +18,7 @@ const mods = [
   FooterComponent,
   MarkdownRendererComponent,
   IntroComponent,
+  SidePanelComponent,
 ];
 
 @NgModule({
