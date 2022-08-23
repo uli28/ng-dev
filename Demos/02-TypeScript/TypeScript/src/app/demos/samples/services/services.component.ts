7c446f3394c808e17345a360acc0ab25f76e4b7c
Alexander Pajer <alexander.pajer@integrations.at>
2022-08-23 08:52:40 +0200

    docs

--- Demos/02-TypeScript/TypeScript/src/app/demos/samples/services/services.component.ts
+++ Demos/02-TypeScript/TypeScript/src/app/demos/samples/services/services.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import axios from 'axios';
 import { environment } from '../../../../environments/environment';
 import { Skill } from '../skills/skill.model';
