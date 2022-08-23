b090c7abd6ed83158c5f473ef43e7a0a164b93b8
Alexander Pajer <alexander.pajer@integrations.at>
2022-08-23 08:48:23 +0200

    linting added

--- Demos/02-TypeScript/TypeScript/src/app/demos/samples/eslint/eslint.component.ts
+++ Demos/02-TypeScript/TypeScript/src/app/demos/samples/eslint/eslint.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eslint',
   templateUrl: './eslint.component.html',
-  styleUrls: ['./eslint.component.scss']
+  styleUrls: ['./eslint.component.scss'],
 })
 export class EslintComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  awaitInArr() {
+    function bar(thing: any) {
+      return thing;
+    }
+
+    async function foo(things) {
+      const results = [];
+      for (const thing of things) {
+        // Bad: each loop iteration is delayed until the entire asynchronous operation completes
+        results.push(await bar(thing));
+      }
+      return results;
+    }
+
+    foo('abc');
   }
-
 }
--- Demos/02-TypeScript/TypeScript/src/assets/markdown/eslint.md
+++ Demos/02-TypeScript/TypeScript/src/assets/markdown/eslint.md
@@ -1,3 +1,27 @@
 - Show [ES Lint Documentations - Rules](https://eslint.org/docs/rules)
 - Show [Angular ES Lint Repo](https://github.com/angular-eslint/angular-eslint)
-- Explain `.eslintrc.json`
+
+Add Linting to project:
+
+```bash
+ng add @angular-eslint/schematics
+```
+
+Point out `"no-await-in-loop":"warn"` in `.eslintrc.json` and explain code in eslint.component.ts:
+
+```typescript
+async function foo(things) {
+    const results = [];
+    for (const thing of things) {
+    // Bad: each loop iteration is delayed until the entire asynchronous operation completes
+    results.push(await bar(thing));
+    }
+    return results;
+}
+```
+
+Run Linter
+
+```bash
+ng lint
+```
