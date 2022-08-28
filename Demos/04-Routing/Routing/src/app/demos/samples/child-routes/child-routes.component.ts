ebf08e94b5e8ba3e35d74dbe60fffa114a11e9d9
Alexander Pajer <alexander.pajer@integrations.at>
2022-08-28 10:42:02 +0200

    completed

--- Demos/04-Routing/Routing/src/app/demos/samples/child-routes/child-routes.component.ts
+++ Demos/04-Routing/Routing/src/app/demos/samples/child-routes/child-routes.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { DemoContainerComponent } from '../../demo-container/demo-container.component';
 import { RouteGuardsComponent } from '../route-guards/route-guards.component';
 import { RoutingBasicsComponent } from '../routing-basics/routing-basics.component';
-import { SecondaryRoutesComponent } from '../secondary-routes/secondary-routes.component';
 import { PreloadComponent } from '../preload/preload.component';
 
 @Component({
@@ -23,7 +22,6 @@ export class ChildRoutesComponent implements OnInit {
         children: [
           { path: 'routingbasics', component: RoutingBasicsComponent },
           { path: 'childroutes', component: ChildRoutesComponent },
-          { path: 'secondary', component: SecondaryRoutesComponent },
           { path: 'routeguards', component: RouteGuardsComponent },
           { path: 'preload', component: PreloadComponent },
         ],
