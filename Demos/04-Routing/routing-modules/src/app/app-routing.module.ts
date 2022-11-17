b6f441e779854a41dee7a4904b1875a0649b174b
Alexander Pajer <alexander.pajer@integrations.at>
2022-11-17 08:38:47 +0100

    migraded including mod 06

--- Demos/04-Routing/routing-modules/src/app/app-routing.module.ts
+++ Demos/04-Routing/routing-modules/src/app/app-routing.module.ts
@@ -60,9 +60,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      enableTracing: false,
-      relativeLinkResolution: 'legacy',
-    }),
+    enableTracing: false
+}),
   ],
   exports: [RouterModule],
 })
