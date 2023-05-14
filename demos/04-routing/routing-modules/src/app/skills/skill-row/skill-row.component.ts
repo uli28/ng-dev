c59381eaaca8b6a84cd489df782c222d22d03af5
Alexander Pajer <alexander.pajer@integrations.at>
2023-05-14 10:40:22 +0200

    testing migrated

--- demos/04-routing/routing-modules/src/app/skills/skill-row/skill-row.component.ts
+++ demos/04-routing/routing-modules/src/app/skills/skill-row/skill-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Skill } from '../skill.model';
 
 @Component({
@@ -6,13 +6,9 @@ import { Skill } from '../skill.model';
   templateUrl: './skill-row.component.html',
   styleUrls: ['./skill-row.component.scss'],
 })
-export class SkillRowComponent implements OnInit {
+export class SkillRowComponent {
   @Input() skill: Skill = new Skill();
   @Output() editItem: EventEmitter<Skill> = new EventEmitter();
-
-  constructor() {}
-
-  ngOnInit(): void {}
 
   edit(item: Skill): void {
     this.editItem.emit(item);
