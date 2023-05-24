ca3db6e82f83f6a339c9a635128eeb31620a228a
Alexander Pajer <alexander.pajer@integrations.at>
2023-05-24 12:48:06 +0200

    day3 changes

--- demos/05-ui/user-interface/src/app/demos/samples/style-inheritance/nested-child/nested-child.component.ts
+++ demos/05-ui/user-interface/src/app/demos/samples/style-inheritance/nested-child/nested-child.component.ts
@@ -1,22 +1,14 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-nested-child',
   template: ` <div class="divclass">Nested Child</div> `,
   styles: [
     `
-           .divclass {
-             border: black 4px solid;
-           }
-     
-           :host-context(.seehowctxworks) {
-             color: blue;
-           }
-         `,
+      .divclass {
+        border: black 4px solid;
+      }
+    `,
   ],
 })
-export class NestedChildComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class NestedChildComponent { }
